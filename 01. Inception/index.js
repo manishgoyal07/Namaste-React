2d3b314d6debf3150b51e96c81b67cc2f23d9ed3
Manish Goyal <manishgoyalbsr7@gmail.com>
2023-10-05 00:11:28 +0530

    :)

--- 01. Inception/index.js
+++ 01. Inception/index.js
@@ -1,10 +1,10 @@
 // To create a react app: 
 
 // 1. Creating a Component
-const MyComponent = React.createElement(
+const MyComponent1 = React.createElement(
     'h1', //Tag Name
     {
-        id: 'header',
+        id: 'header1',
         style: {
             backgroundColor: "aqua",
             color: "Red"
@@ -12,7 +12,24 @@ const MyComponent = React.createElement(
     },
     "Namaste World" //Child Component
 );
+const MyComponent2 = React.createElement(
+    'h2', //Tag Name
+    {
+        id: 'header2',
+        style: {
+            backgroundColor: "Red",
+            color: "Yellow"
+        } //Props
+    },
+    "Namaste React" //Child Component
+);
+
 // 2. Creating a Root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// 3. Rendering child into the Root
-root.render(MyComponent);
+
+// 3. Rendering child into the Root (If there are multiple siblings/Child components, put them into an array)
+// root.render(MyComponent1); //In case of a Single child component
+root.render([MyComponent1, MyComponent2]);
+
+// Every React element is an object at the end of the day
+console.log(MyComponent1); //Object
